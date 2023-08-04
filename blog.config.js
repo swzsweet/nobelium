f833a57ca1d9b9b5d332ff86125d1e85c7e4e5d8
swzsweet <437430243@qq.com>
2023-08-04 14:57:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'swz 的 Blog',
   author: 'swz',
   email: '',
-  link: 'https://nobelium.vercel.app',
+  link: 'http://blog.shiwz.com',
   description: 'swz 的个人博客',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
@@ -17,7 +17,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Blogger_icon_2017.svg', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/sweet_swz9983',
   seo: {
     keywords: ['Blog', 'swz'],
